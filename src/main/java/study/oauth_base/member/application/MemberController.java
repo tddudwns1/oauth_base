e041278ec549a670543bd929e823d53daef126ca
tddudwns1 <tjddudwns1@naver.com>
2024-01-21 18:25:17 +0900

    :sparkles: 테스트용 유저 컨트롤러 생성

---
+++ src/main/java/study/oauth_base/member/application/MemberController.java
@@ -0,0 +1,32 @@
+package study.oauth_base.member.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.oauth_base.authentication.domain.AuthTokensGenerator;
+import study.oauth_base.member.domain.Member;
+import study.oauth_base.member.domain.MemberRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+    private final MemberRepository memberRepository;
+    private final AuthTokensGenerator authTokensGenerator;
+
+    @GetMapping
+    public ResponseEntity<List<Member>> findAll() {
+        return ResponseEntity.ok(memberRepository.findAll());
+    }
+
+    @GetMapping("/{accessToken}")
+    public ResponseEntity<Member> findByAccessToken(@PathVariable String accessToken) {
+        Long memberId = authTokensGenerator.extractMemberId(accessToken);
+        return ResponseEntity.ok(memberRepository.findById(memberId).get());
+    }
+}
