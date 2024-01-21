ab87591fcde5999d704f8a9447ac1f1a0565ff97
tddudwns1 <tjddudwns1@naver.com>
2024-01-21 15:10:47 +0900

    :sparkles: 프로젝트 생성
    application 파일 설정(oauth, db)

---
+++ src/main/java/study/oauth_base/OauthBaseApplication.java
@@ -0,0 +1,13 @@
+package study.oauth_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/oauth_base/OauthBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package study.oauth_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
