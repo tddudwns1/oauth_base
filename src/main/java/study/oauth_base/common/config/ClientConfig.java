afaee89876cd1ff444fe79ecab6b4c48896b709c
tddudwns1 <tjddudwns1@naver.com>
2024-01-21 16:35:04 +0900

    :ambulance: 누락 어노테이션 추가

--- src/main/java/study/oauth_base/common/config/ClientConfig.java
+++ src/main/java/study/oauth_base/common/config/ClientConfig.java
@@ -1,8 +1,10 @@
 package study.oauth_base.common.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class ClientConfig {
 
     @Bean
