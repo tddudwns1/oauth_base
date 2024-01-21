217af02ae88f4d507584e578903b6b6439d77afd
tddudwns1 <tjddudwns1@naver.com>
2024-01-21 16:02:57 +0900

    :sparkles: JWT(Access Token) 생성
    클라이언트에게 Access Token 을 내려주어야 함
    OAuth 플랫폼들의 Access Token 을 클라이언트에게 내려주면 플랫폼 별로 만료 기간 관리도 번거롭고 혹여나 탈취라도 당하면 안되기 때문에 반드시 직접 토큰을 만들어서 내려 줌
    
    1. JwtTokenProvider
    JWT 토큰을 만들어주는 유틸 클래스
    2. AuthTokens
    사용자에게 내려주는 서비스의 인증 토큰 값
    3. AuthTokensGenerator
    AuthTokens 을 발급해주는 클래스
    generate: memberId (사용자 식별값) 을 받아 Access Token 을 생성
    extractMemberId: Access Token 에서 memberId (사용자 식별값) 추출

---
+++ src/main/java/study/oauth_base/authentication/domain/AuthTokens.java
@@ -0,0 +1,20 @@
+package study.oauth_base.authentication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthTokens {
+    private String accessToken;
+    private String refreshToken;
+    private String grantType;
+    private Long expiresIn;
+
+    public static AuthTokens of(String accessToken, String refreshToken, String grantType, Long expiresIn) {
+        return new AuthTokens(accessToken, refreshToken, grantType, expiresIn);
+    }
+}
---
+++ src/main/java/study/oauth_base/authentication/domain/AuthTokensGenerator.java
@@ -0,0 +1,33 @@
+package study.oauth_base.authentication.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import study.oauth_base.authentication.infra.JwtTokenProvider;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokensGenerator {
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthTokens generate(Long memberId) {
+        long now = (new Date()).getTime();
+        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        String subject = memberId.toString();
+        String accessToken = jwtTokenProvider.generate(subject, accessTokenExpiredAt);
+        String refreshToken = jwtTokenProvider.generate(subject, refreshTokenExpiredAt);
+
+        return AuthTokens.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME / 1000L);
+    }
+
+    public Long extractMemberId(String accessToken) {
+        return Long.valueOf(jwtTokenProvider.extractSubject(accessToken));
+    }
+}
