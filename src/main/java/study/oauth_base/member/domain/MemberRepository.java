80b199b2110988bc911154dfed1e1fde47067fd0
tddudwns1 <tjddudwns1@naver.com>
2024-01-21 16:06:30 +0900

    :sparkles: 유저 레포지토리 생성

---
+++ src/main/java/study/oauth_base/member/domain/MemberRepository.java
@@ -0,0 +1,9 @@
+package study.oauth_base.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
